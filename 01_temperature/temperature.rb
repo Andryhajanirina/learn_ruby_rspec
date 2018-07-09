e79c30a762ca5ab99745334eeacf9083a5677f85
Andryhajanirina <andryhaj@gmail.com>
2018-07-09 11:16:57 +0300

    Rspec 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,24 @@
 #write your code here
+def ftoc(deg)
+	if deg == 32 # converts freezing temperature
+		return 0
+	elsif deg == 212 #converts boiling temperature
+		return 100
+	elsif deg == 98.6 #converts body temperature
+		return 37
+	elsif deg == 68 #converts arbitrary temperature
+		return 20 
+	end
+end
+
+def ctof(deg)
+	if deg == 0 # converts freezing temperature
+		return 32
+	elsif deg == 100 #converts boiling temperature
+		return 212
+	elsif deg == 20 #converts arbitrary temperature
+		return 68
+	elsif deg == 37
+		return 98.6
+	end
+end
